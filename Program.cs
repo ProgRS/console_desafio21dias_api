15bc275957e50d9f00133fbe9eb5ba2966793011
Luis Fernando Trindade <lfernandotc@hotmail.com>
2024-05-01 15:12:33 -0300

    Uso do while e for

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace console_desafio21dias_api
 {
@@ -6,21 +7,42 @@ namespace console_desafio21dias_api
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite o primeiro numero:");
-            var numeroUm = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite o segundo  numero:");
-            int numeroDois = Convert.ToInt32(Console.ReadLine());
-            var soma = numeroUm * numeroDois;
+            Console.WriteLine("Digite numero inicial:");
+            var numeroIncial = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o numero dois:");
+            int numeroFinal = Convert.ToInt32(Console.ReadLine());
+            int indice = numeroIncial;
+            int[] colecaoPar = new int[numeroFinal - numeroIncial];
+            int[] colecaoImpar = new int[numeroFinal - numeroIncial];
+            int indicePar = 0;
+            int indiceImpar = 0;
 
-            Console.WriteLine($"Digite o numero premiado?");
-            int resultado = Convert.ToInt32(Console.ReadLine());
-            if (resultado == soma || resultado == 1 || resultado == 10 || (resultado >= 100 && resultado <= 200))
-                Console.WriteLine($"Parabéns você acertou o resultado: {soma}");
-            else
-                Console.WriteLine($"O resultado é {soma}, você errou, tente novamente na proxima");
+            while (indice < numeroFinal){
 
+                if(indice % 2 == 0){
+                    colecaoPar[indicePar] = indice;
+                    indicePar++;
+                    //Console.WriteLine($"O numero {indice} é par");
+                }
+                else{
+                    colecaoImpar[indiceImpar] = indice;
+                    indiceImpar++;
+                    //Console.WriteLine($"O numero {indice} é impar");
+                }
+                indice++;                
+                
+            }
 
-            //Console.WriteLine($"A soma das variaveis é : {soma}");
+            Console.WriteLine("Os números pares são: ");
+            for(int count=0 ; count < indicePar; count++){
+
+                Console.WriteLine(colecaoPar[count]);
+            }
+            Console.WriteLine("Os números impares são: ");
+            for (int count = 0; count < indicePar; count++)
+            {
+                Console.WriteLine(colecaoImpar[count] );
+            }
 
         }
     }
