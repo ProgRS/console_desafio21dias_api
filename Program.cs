d0386d974a0e53ac428befb1025aaade8ed70002
Luis Fernando Trindade <lfernandotc@hotmail.com>
2024-05-01 06:17:26 -0300

    inclusão operadores logicos

--- Program.cs
+++ Program.cs
@@ -8,16 +8,16 @@ namespace console_desafio21dias_api
         {
             Console.WriteLine("Digite o primeiro numero:");
             var numeroUm = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite o primeiro numero:");
+            Console.WriteLine("Digite o segundo  numero:");
             int numeroDois =  Convert.ToInt32(Console.ReadLine());
-            var soma = numeroUm + numeroDois;
+            var soma = numeroUm * numeroDois;
 
-            Console.WriteLine($"Digite o resultado da soma do {numeroUm} + {numeroDois}");
+            Console.WriteLine($"Digite o numero premiado?");
             int resultado = Convert.ToInt32(Console.ReadLine());
-                if(resultado == soma)
-                    Console.WriteLine("Parabéns você acertou");
+                if(resultado == soma || resultado == 1 || resultado == 10 || (resultado >= 100 && resultado <=200))
+                    Console.WriteLine($"Parabéns você acertou o resultado: {soma}");
                 else
-                    Console.WriteLine("Desculpe você errou");
+                    Console.WriteLine($"O resultado é {soma}, você errou, tente novamente na proxima");
                 
 
             //Console.WriteLine($"A soma das variaveis é : {soma}");
